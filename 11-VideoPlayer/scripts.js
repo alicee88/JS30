bf16464de8c1ba3ad18401854d24ebe28d0684e6
Alice <alice.easey@gmail.com>
2020-01-17 08:55:00 +0000

    Add scrubber and progress controls

--- 11-VideoPlayer/scripts.js
+++ 11-VideoPlayer/scripts.js
@@ -1,13 +1,19 @@
 const player = document.querySelector('.player__video');
 const playButton = document.querySelector('.player__button');
 const volumeControl = document.querySelector('input[name="volume"');
+const scrubberFullWidth = document.querySelector('.progress');
+const scrubber = document.querySelector('.progress__filled');
 
 playButton.addEventListener('click', handlePlayButton);
+
 volumeControl.addEventListener('change', handleVolume);
+volumeControl.addEventListener('mousemove', handleVolume);
+scrubberFullWidth.addEventListener('click', handleProgress);
 
 async function playVideo() {
     try {
         playButton.textContent = '❙❙';
+    
         await player.play();
     } catch(err) {
         console.log('Could not play video');
@@ -27,3 +33,19 @@ function handlePlayButton() {
 function handleVolume() {
     player.volume = this.value;
 }
+
+function handleProgress(e) {
+    let mousePos = e.clientX - scrubberFullWidth.getBoundingClientRect().x;
+    let scrubberRatio = mousePos / scrubberFullWidth.offsetWidth;
+    let mousePercentage = scrubberRatio * 100;
+    scrubber.style.setProperty('flex-basis', `${mousePercentage}%`);
+    player.currentTime = player.duration * scrubberRatio;
+}
+
+function updateScrubber() {
+    let playedTime = (player.currentTime / player.duration) * 100;
+    scrubber.style.setProperty('flex-basis', `${playedTime}%`);
+}
+
+setInterval(updateScrubber, 500);
+
